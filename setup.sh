524ee9dd257583a7d132b1f18611f1d9f69c8835
neko-0 <yuzhan.physics@gmail.com>
2022-11-09 11:42:57 -0800

    fixed setup for cmake order

--- setup.sh
+++ setup.sh
@@ -4,13 +4,10 @@ export TestArea=$(pwd)
 export WAVEANA_BUILD_DIR=$TestArea/../WaveformAna_build
 
 mkdir -p $WAVEANA_BUILD_DIR
-# alias build='cmake --build $TestArea/../build; source $TestArea/../build/*/setup.sh'
-alias build='cmake --build $WAVEANA_BUILD_DIR'
-
+cd $WAVEANA_BUILD_DIR
 cmake -B $WAVEANA_BUILD_DIR -S $TestArea
 
-cd $WAVEANA_BUILD_DIR
-
+alias build='cmake --build $WAVEANA_BUILD_DIR'
 # there must be a easier way to set alias for executable
 alias run_Ana='$WAVEANA_BUILD_DIR/analysisDriver/run_Ana'
 alias ls_Ana='$WAVEANA_BUILD_DIR/analysisDriver/ls_Ana'
