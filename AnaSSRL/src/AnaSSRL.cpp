e28866497cfcc0f2ebc45035cc268a2c16d765b0
neko-0 <yuzhan.physics@gmail.com>
2023-05-18 14:58:34 -0700

    update SSRL ana

--- AnaSSRL/src/AnaSSRL.cpp
+++ AnaSSRL/src/AnaSSRL.cpp
@@ -5,16 +5,46 @@
 #include <string>
 #include <omp.h>
 
+#include "yaml-cpp/yaml.h"
+
 namespace wm = waveform_methods;
 
 void AnaSSRL::initialize(BetaConfigMgr* const configMgr){
 
-  for(int i = 0; i < this->num_ch_; i++){
+  LOG_INFO("external config: " + configMgr->ext_config_name());
+  YAML::Node yaml_config = YAML::LoadFile(configMgr->ext_config_name());
+
+  const auto &general = yaml_config["general"];
+  this->store_waveform = general["store_waveform"].as<bool>();
+  this->use_single_t_trace = general["use_single_t_trace"].as<bool>();
+  this->use_single_input_t_trace = general["use_single_input_t_trace"].as<bool>();
+  int num_ch = this->num_ch_;
+  if(general["nchannels"].as<int>() > 0){
+    num_ch = general["nchannels"].as<int>();
+  }
+
+  const auto &buckets = yaml_config["buckets"];
+  this->bucket_t_start_ = buckets["bucket_t_start"].as<double>();
+  this->bucket_t_end_ = buckets["bucket_t_end"].as<double>();
+  this->nbuckets_ = buckets["nbuckets"].as<int>();
+
+  const auto &fix_win = yaml_config["fix_window"];
+  this->fill_fix_window = fix_win["fill_fix_window"].as<bool>();
+  this->fix_win_start_ = fix_win["fix_win_start"].as<double>();
+  this->fix_win_step_size_ = fix_win["fix_win_step_size"].as<double>();
+  this->fix_win_nstep_ = fix_win["fix_win_nstep"].as<int>();
+
+  for(int i = 0; i < num_ch_; i++){
     // input branches
     std::string current_ch = std::to_string(ch_start_ + i);
     w[i] = configMgr->SetInputBranch<std::vector<double>>("w" + current_ch);
     if(!w[i]) continue;
-    t[i] = configMgr->SetInputBranch<std::vector<double>>("t" + current_ch);
+    if(!use_single_input_t_trace){
+      t[i] = configMgr->SetInputBranch<std::vector<double>>("t" + current_ch);
+    } else{
+        if(i==0) t[i] = configMgr->SetInputBranch<std::vector<double>>("time");
+        else t[i] = t[0];
+    }
 
     active_ch_.push_back(i);
 
@@ -68,13 +98,20 @@ void AnaSSRL::initialize(BetaConfigMgr* const configMgr){
     }
   }
 
-  LOG_INFO("number of active chanenls: " + std::to_string(active_ch_.size()));
+  if(fill_fix_window) prepare_fix_window_branches(configMgr);
 
-  LOG_INFO("external config: " + configMgr->ext_config_name());
+  // trig = configMgr->SetInputBranch<std::vector<double>>("trg0");
+  // trig_time = configMgr->SetOutputBranch<double>("trig_time");
+
+  LOG_INFO("number of active chanenls: " + std::to_string(active_ch_.size()));
 }
 
 void AnaSSRL::execute(BetaConfigMgr* const configMgr){
-  // #pragma omp parallel for
+  // auto timestamp = wm::FindTimeAtThreshold(*trig, *t[0], 3000);
+  // if(timestamp.size()>0) *trig_time = timestamp.at(0);
+  // else *trig_time = -1.0;
+
+  // #pragma omp parallel for if(active_ch_.size() > 5)
   for(auto &ch : active_ch_){
     if(w[ch]->size() == 0){
       LOG_WARNING("Trace size 0");
@@ -82,34 +119,33 @@ void AnaSSRL::execute(BetaConfigMgr* const configMgr){
     }
 
     // auto corr_w = wm::Baseline::ARPLS_PLS(*w[ch], 1.0e13);
-    int win_size = w[ch]->size() / 12;
-    auto corr_w = wm::Baseline::NoiseMedian(*w[ch], win_size);
+    auto corr_w = wm::Baseline::NoiseMedian(*w[ch], w[ch]->size() / 12);
 
     auto mix_params = wm::CalcMaxNoiseBase(*w[ch], 0.25);
+    // auto range_rms = wm::CalcNoise(*w[ch], *t[ch], 480, 550);
+    // mix_params.rms = range_rms;
 
     // check polarity of the signal. determine threshold for pmax finder
-    double polarity;
-    // double abs_max;
-    if(mix_params.pos_max > mix_params.neg_max*-1.0){
-      polarity = 1.0;
-      // abs_max = mix_params.pos_max;
-    } else {
-      polarity = -1.0;
-      // abs_max = -1.0*mix_params.neg_max;
-    }
-    double threshold = 5.0 * mix_params.rms;
+    double polarity = -1.0; // forcing signal inverstion
+    // if(std::abs(mix_params.pos_max) >= std::abs(mix_params.neg_max)) {
+    //   polarity = 1.0;
+    // } else {
+    //   polarity = -1.0;
+    // }
+    // double threshold = 5.0 * mix_params.rms;
+    double threshold = 5.0 * 6.0; // AC Digitizer Run
 
     for(int i=0; i < w[ch]->size(); i++){
       w[ch]->at(i) = (w[ch]->at(i) - mix_params.baseline)*polarity;
       corr_w.at(i) *= polarity;
-      // corr_w.at(i) = (corr_w.at(i)+w[ch]->at(i)) / 2.0; // look good with average
     }
 
     auto n_wave_pts = wm::FindMultipleSignalMax(corr_w, *t[ch], threshold);
-    auto n_raw_wave_pts = wm::FindMultipleSignalMax(*w[ch], *t[ch], threshold);
+    auto n_raw_wave_pts = wm::FindMultipleSignalMax(w[ch], t[ch], threshold);
 
     *output_basecorr[ch] = wm::Baseline::MultiSignalBaselineCorrection(
-      n_raw_wave_pts, *w[ch], *t[ch], 0.5, 5e-9, 5e-9);
+      n_raw_wave_pts, w[ch], t[ch], 0.5, 5e-9, 5e-9
+    );
 
     // std::move(cfd_times.begin(), cfd_times.end(), std::back_inserter(*output_cfd[ch]));
     // #pragma omp parallel for
@@ -139,7 +175,15 @@ void AnaSSRL::execute(BetaConfigMgr* const configMgr){
       output_raw_pmax[ch]->push_back(pt.v);
     }
 
-    bucket_time_difference(ch, -44e-9, 2.08e-9);
+    bucket_time_difference(ch, bucket_t_start_, bucket_t_end_); // 0.5, 1.0 for Digitizer run
+
+    if(fill_fix_window){
+      for(std::size_t _step = 0; _step < fix_win_nstep_; _step++){
+        auto _begin = fix_win_start_ + _step * fix_win_step_size_;
+        auto _end = _begin + fix_win_step_size_;
+        fill_fix_window_branches(ch, corr_w, *t[ch], _begin, _end);
+      }
+    }
 
     if(store_waveform){
       std::move(corr_w.begin(), corr_w.end(), std::back_inserter(*output_corr_w[ch]));
@@ -217,5 +261,37 @@ void AnaSSRL::bucket_time_difference(
       output_bucket_cfd20_diff[ch]->emplace_back(cfd20_diff);
       output_bucket_cfd50_diff[ch]->emplace_back(cfd50_diff);
     }
+  }
+}
+
+// =============================================================================
+void AnaSSRL::prepare_fix_window_branches(BetaConfigMgr* const configMgr){
+  for(auto &i : active_ch_){
+    std::string current_ch = std::to_string(i+1);
+    output_fix_pmax[i] = configMgr->SetOutputBranch<std::vector<float>>("fix_pmax" + current_ch);
+    output_fix_tmax[i] = configMgr->SetOutputBranch<std::vector<float>>("fix_tmax" + current_ch);
+    output_fix_area[i] = configMgr->SetOutputBranch<std::vector<float>>("fix_area" + current_ch);
+  }
+}
+
+// =============================================================================
+void AnaSSRL::fill_fix_window_branches(
+  int ch,
+  const std::vector<double> &v_trace,
+  const std::vector<double> &t_trace,
+  double t_min,
+  double t_max)
+{
+  double threshold = 0.0;
+  auto s_max = wm::FindSignalMax(v_trace, t_trace, t_min, t_max);
+  if(s_max.v > threshold){
+    output_fix_pmax[ch]->push_back(s_max.v);
+    output_fix_tmax[ch]->push_back(s_max.t);
+    output_fix_area[ch]->push_back(wm::CalcPulseArea(v_trace, t_trace, s_max.index));
+  }
+  else{
+    output_fix_pmax[ch]->push_back(0.0);
+    output_fix_tmax[ch]->push_back(-1.0);
+    output_fix_area[ch]->push_back(0.0);
   }
 }
