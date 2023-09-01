2afa1e777c7c15a62785a8a2102db6d9553bd15c
neko-0 <yuzhan.physics@gmail.com>
2023-09-01 11:53:35 -0700

    cleanup for G4 ana

--- AnaG4/AnaG4/AnaG4.hpp
+++ AnaG4/AnaG4/AnaG4.hpp
@@ -11,86 +11,45 @@ struct AnaG4 : BaseAna {
   AnaG4(){};
   ~AnaG4(){};
 
+  virtual void setup(BetaConfigMgr* const configMgr);
   virtual void initialize(BetaConfigMgr* const configMgr);
-  virtual void execute(BetaConfigMgr* const configMgr);
   virtual void finalize(BetaConfigMgr* const configMgr);
+  virtual bool execute(BetaConfigMgr* const configMgr);
 
 private:
-  const int ch_start_ = 0;
-  static const int num_ch_ = 16;
-  std::vector<int> active_ch_ = {};
-  bool store_waveform = true;
-  bool use_single_t_trace = true;
-  bool use_single_input_t_trace = true;
-  bool found_single_t_trace = false;
-
-  double bucket_t_start_ = -44e-9;
-  double bucket_t_end_ = 2.08e-9;
-  int nbuckets_= 28;
-
-  bool fill_fix_window = true;
-  double fix_win_start_ = 560.0;
-  double fix_win_step_size_ = 11;
-  int fix_win_nstep_ = 40;
-
-  bool do_max_ch_ = false;
-
-  int baseline_opt = 0;
-  int run_type = 0;
-
   // ===========================================================================
   // input variables
-  std::vector<double> *w[num_ch_];
-  std::vector<double> *t[num_ch_];
+  std::vector<double> *pion_x;
+  std::vector<double> *pion_y;
+  std::vector<double> *pion_z;
+  std::vector<double> *pion_edep;
+  std::vector<int> *pion_region;
+  double *pion_ke;
 
-  std::vector<double> *trig;
+  std::vector<double> *muon_x;
+  std::vector<double> *muon_y;
+  std::vector<double> *muon_z;
+  std::vector<double> *muon_edep;
+  std::vector<int> *muon_region;
+  double *muon_ke;
 
   // ===========================================================================
   // output variables
-  bool *output_basecorr[num_ch_];
-  int *output_nsignal[num_ch_];
-  float *output_rms[num_ch_];
+  double *output_pion_last_x;
+  double *output_pion_last_y;
+  double *output_pion_last_z;
+  double *output_pion_last_edep;
+  std::vector<double> *output_pion_z;
+  std::vector<double> *output_pion_edep;
+  // std::vector<int> *output_pion_region;
 
-  std::vector<float> *output_rise[num_ch_];
-  std::vector<float> *output_area[num_ch_];
-  std::vector<float> *output_fwhm[num_ch_];
-  std::vector<float> *output_20cfd[num_ch_];
-  std::vector<float> *output_50cfd[num_ch_];
-  std::vector<float> *output_pmax[num_ch_];
-  std::vector<float> *output_tmax[num_ch_];
-  std::vector<float> *output_tmax_diff[num_ch_];
-  std::vector<float> *output_raw_pmax[num_ch_];
-
-  std::vector<float> *output_bucket_pmax[num_ch_];
-  std::vector<float> *output_bucket_area[num_ch_];
-  std::vector<float> *output_bucket_tmax[num_ch_];
-  std::vector<float> *output_bucket_cfd20[num_ch_];
-  std::vector<float> *output_bucket_cfd50[num_ch_];
-  std::vector<int> *output_bucket_index[num_ch_];
-  std::vector<float> *output_bucket_tmax_diff[num_ch_];
-  std::vector<float> *output_bucket_cfd20_diff[num_ch_];
-  std::vector<float> *output_bucket_cfd50_diff[num_ch_];
-  float *output_bucket_corr[num_ch_];
-
-  std::vector<int> *output_max_ch;
-  std::vector<int> *output_2nd_max_ch;
-  std::vector<int> *output_small_pad_max_ch;
-
-  std::vector<double> *output_sum_large;
-  std::vector<double> *output_sum_small;
-  std::vector<double> *output_sum_strip_set1; // [12, 5, 6]
-  std::vector<double> *output_sum_strip_set2; // [11, 7, 7]
-  std::vector<double> *output_sum_strip_set3; // [11, 7, 7]
-
-  std::vector<float> *output_w[num_ch_];
-  std::vector<float> *output_corr_w[num_ch_];
-  std::vector<float> *output_t[num_ch_];
-
-  std::vector<float> *output_fix_pmax[num_ch_];
-  std::vector<float> *output_fix_tmax[num_ch_];
-  std::vector<float> *output_fix_area[num_ch_];
-
-  double *trig_time;
+  double *output_muon_last_x;
+  double *output_muon_last_y;
+  double *output_muon_last_z;
+  double *output_muon_last_edep;
+  std::vector<double> *output_muon_z;
+  std::vector<double> *output_muon_edep;
+  // std::vector<int> *output_muon_region;
 
 };
 
