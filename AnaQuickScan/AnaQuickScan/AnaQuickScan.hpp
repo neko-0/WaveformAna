b4f81658113b5865a7f5f254cd11dd9f83e80da1
neko-0 <yuzhan.physics@gmail.com>
2024-03-01 21:11:22 -0800

    update quick scan for heatmap

--- AnaQuickScan/AnaQuickScan/AnaQuickScan.hpp
+++ AnaQuickScan/AnaQuickScan/AnaQuickScan.hpp
@@ -4,6 +4,8 @@
 #include "baseAna/baseAna.hpp"
 #include "configMgr/betaConfigMgr.hpp"
 
+#include <TProfile2D.h>
+
 #include <vector>
 
 struct AnaQuickScan : BaseAna {
@@ -23,24 +25,13 @@ private:
   // input variables
   std::vector<double> *w[num_ch_];
   std::vector<double> *t;
-  double *posx = 0;
-  double *posy = 0;
-  double *posz = 0;
- 
-  double **posx_addr;
+  std::vector<double> *pos;
 
   // output variables
   double *output_pmax[num_ch_];
-  double *output_tmax[num_ch_];
-  double *output_rise[num_ch_];
-  double *output_area[num_ch_];
-  double *output_fwhm[num_ch_];
-  double *output_rms[num_ch_];
   double *output_x, *output_y, *output_z;
 
-  std::vector<double> *output_w[num_ch_];
-  std::vector<double> *output_t;
-  // std::vector<double> *output_cfd[num_ch_];
+  TProfile2D *heatmap[num_ch_];
 };
 
 #endif
--- AnaQuickScan/src/AnaQuickScan.cpp
+++ AnaQuickScan/src/AnaQuickScan.cpp
@@ -5,39 +5,50 @@
 #include <string>
 #include <iostream>
 
+#include "yaml-cpp/yaml.h"
+
 namespace wm = waveform_methods;
 
 void AnaQuickScan::setup(BetaConfigMgr* const configMgr) { }
 
-void AnaQuickScan::initialize(BetaConfigMgr* const configMgr){
+void AnaQuickScan::initialize(BetaConfigMgr* const configMgr){  
 
-  t = configMgr->SetInputBranch<std::vector<double>>("t0");
+  int x_bins = 1000;
+  double x_min = -1e4;
+  double x_max = 1e4;
 
-  posx = configMgr->SetInputBranch<double>("x");
-  posy = configMgr->SetInputBranch<double>("y");
-  posz = configMgr->SetInputBranch<double>("z");
+  int y_bins = 1000;
+  double y_min = -1e4;
+  double y_max = 1e4;
 
-  output_t = configMgr->SetOutputBranch<std::vector<double>>("t");
-  output_x = configMgr->SetOutputBranch<double>("x");
-  output_y = configMgr->SetOutputBranch<double>("y");
-  output_z = configMgr->SetOutputBranch<double>("z");
+  double z_min = -1e4;
+  double z_max = 1e4;
+  
+  if( !configMgr->ext_config_name().empty() ){
+    LOG_INFO("external config: " + configMgr->ext_config_name());
+    YAML::Node yaml_config = YAML::LoadFile(configMgr->ext_config_name());
+    const auto ranges = yaml_config["ranges"];
+    x_bins = ranges["x_bins"].as<int>();
+    x_min = ranges["x_min"].as<double>();
+    x_max = ranges["x_max"].as<double>();
+    y_bins = ranges["y_bins"].as<int>();
+    y_min = ranges["y_min"].as<double>();
+    y_max = ranges["y_max"].as<double>();
+    z_min = ranges["z_min"].as<double>();
+    z_max = ranges["z_max"].as<double>();
+  }
+
+  pos = configMgr->SetInputBranch<std::vector<double>>("pos");
+  t = configMgr->SetInputBranch<std::vector<double>>("time");
 
   for(int i = 0; i < this->num_ch_; i++){
     // input branches
     std::string current_ch = std::to_string(ch_start_ + i);
     w[i] = configMgr->SetInputBranch<std::vector<double>>("w" + current_ch);
-
-    // output branches
-    output_pmax[i] = configMgr->SetOutputBranch<double>("pmax" + current_ch);
-    output_tmax[i] = configMgr->SetOutputBranch<double>("tmax" + current_ch);
-    output_rise[i] = configMgr->SetOutputBranch<double>("rise" + current_ch);
-    output_area[i] = configMgr->SetOutputBranch<double>("area" + current_ch);
-    output_fwhm[i] = configMgr->SetOutputBranch<double>("fwhm" + current_ch);
-    output_rms[i] = configMgr->SetOutputBranch<double>("rms" + current_ch);
-
-    // output_cfd[i] = configMgr->SetOutputBranch<std::vector<double>>("cfd" + current_ch);
-    output_w[i] = configMgr->SetOutputBranch<std::vector<double>>("w" + current_ch);
+    std::string pname = "prof" + current_ch;
+    heatmap[i] = new TProfile2D(pname.c_str(), pname.c_str(), x_bins, x_min, x_max, y_bins, y_min, y_max, z_min, z_max);
   }
+
 }
 
 bool AnaQuickScan::execute(BetaConfigMgr* const configMgr){
@@ -45,44 +56,31 @@ bool AnaQuickScan::execute(BetaConfigMgr* const configMgr){
   const double v_scale = 1.0; // V to mV
   const double t_scale = 1.0; // s to ps
 
-  std::move(t->begin(), t->end(), std::back_inserter(*output_t));
-  // std::cout << "posx " << *posx << "\n";
-  *output_x = *posx;
-  *output_y = *posy;
-  *output_z = *posz;
-
   for(int ch = 0; ch < this->num_ch_; ch++){
     int trace_size = w[ch]->size();
-    double baseline = wm::Baseline::CalcBaseline(*w[ch], 0, (int)(0.25*trace_size));
+    double baseline = wm::Baseline::CalcBaseline(*w[ch], *t, 240, 340);
     // inverting signal
     for(int i=0; i < trace_size; i++){
       w[ch]->at(i) -= baseline;
       w[ch]->at(i) *= -1.0*v_scale;
       // t[ch]->at(i) *= t_scale;
-      output_w[ch]->push_back(w[ch]->at(i));
-      // output_t[ch]->push_back(t[ch]->at(i));
     }
 
-    auto wave_pt = wm::FindSignalMax(*w[ch], *t);
-    auto rise = wm::CalcRiseTime(*w[ch], *t, wave_pt.index);
-    auto area = wm::CalcPulseArea(*w[ch], *t, wave_pt.index);
-    auto fwhm = wm::CalcFWHM(*w[ch], *t, wave_pt.index);
-    auto rms = wm::CalcNoise(*w[ch], 0.25);
-    // auto cfd_times = wm::CalcCFDTime(*w[ch], *t, wave_pt.index, 0.1, 0.1);
-
-    *output_pmax[ch] = wave_pt.v;
-    *output_tmax[ch] = wave_pt.t;
-    *output_rise[ch] = rise;
-    *output_area[ch] = area;
-    *output_fwhm[ch] = fwhm;
-    *output_rms[ch] = rms;
-
-    // std::move(cfd_times.begin(), cfd_times.end(), std::back_inserter(*output_cfd[ch]));
+    auto wave_pt = wm::FindSignalMax(*w[ch], *t, 600, 800);
+    // std::cout << "max : " << wave_pt.v << ", " << baseline << "\n";
+    auto f = heatmap[ch]->Fill(pos->at(0), pos->at(1), wave_pt.v, 1);
+    // auto f = heatmap[ch]->Fill(pos->at(0), pos->at(1), 1e3, 1);
+    // std::cout << ch << "ch, " << "max : " << wave_pt.v << ", " << pos->at(0) << ", " << pos->at(1) << ", " << f << "\n";
   }
 
-  return true;
+  return false;
 }
 
 void AnaQuickScan::finalize(BetaConfigMgr* const configMgr){
-  // pass
+
+  configMgr->GetOutputFile()->cd();
+  for(int ch = 0; ch < this->num_ch_; ch++) {
+    heatmap[ch]->Write();
+    delete heatmap[ch];
+  }
 }
