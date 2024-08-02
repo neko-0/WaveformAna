d9c43eda3feec873d22023d6a816c6395a8abe52
neko-0 <yuzhan.physics@gmail.com>
2024-08-01 20:34:32 -0700

    update: no longer change directory when running setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-export TestArea=$(pwd)
-export WAVEANA_BUILD_DIR=$TestArea/../WaveformAna_build
+export CURRENT_PATH=$(pwd)
+export SRC_AREA=$(dirname $BASH_SOURCE)
+export WAVEANA_BUILD_DIR=$SRC_AREA/../WaveformAna_build
+
+echo "Found source directory: ${SRC_AREA}"
+echo "Setting up build directory: ${WAVEANA_BUILD_DIR}"
 
 mkdir -p $WAVEANA_BUILD_DIR
 cd $WAVEANA_BUILD_DIR
-cmake -B $WAVEANA_BUILD_DIR -S $TestArea -DCMAKE_BUILD_TYPE=RELEASE
+cmake -B $WAVEANA_BUILD_DIR -S $SRC_AREA -DCMAKE_BUILD_TYPE=RELEASE
+cd $CURRENT_PATH
 
 alias build='cmake --build $WAVEANA_BUILD_DIR'
 # there must be a easier way to set alias for executable
