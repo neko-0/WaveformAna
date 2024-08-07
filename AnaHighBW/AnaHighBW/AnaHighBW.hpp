a76bca2a078827251ab8123a941468f47eec6f3c
neko-0 <yuzhan.physics@gmail.com>
2024-08-07 14:27:10 -0700

    update: working routine

--- AnaHighBW/AnaHighBW/AnaHighBW.hpp
+++ AnaHighBW/AnaHighBW/AnaHighBW.hpp
@@ -16,14 +16,29 @@ struct AnaHighBW : BaseAna {
   virtual void finalize(BetaConfigMgr* const configMgr);
   virtual bool execute(BetaConfigMgr* const configMgr);
 
-private:
+private: 
+
+    int ch_start_ = 1;
+    int dut_ch_ = 1;
+    std::vector<int> enbale_ch_;
+    std::vector<int> active_ch_;
+    double winsize_ = 0.25e-6;
+    double peak_finding_threshold_ = 0.0015;
     
     // ===========================================================================
     // input waveform channels
     static const int num_ch_ = 4;
-    std::vector<double> *w[num_ch_];
-    std::vector<double> *t[num_ch_];
+    std::vector<double> *i_w[num_ch_];
+    std::vector<double> *i_t[num_ch_];
 
+    std::vector<double> *o_w[num_ch_];
+    std::vector<double> *o_t[num_ch_];
+    
+    std::vector<double> *dut_process_time;
+    std::vector<double> *dut_process_voltage;
+    
+    std::vector<double> *w_step;
+    std::vector<double> *t_step;
 };
 
 #endif
